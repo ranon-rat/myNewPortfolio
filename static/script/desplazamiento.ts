f268e461ff48bc48270e1ba8537ef330d086101e
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-08-11 03:22:36 -0500

    my new portafolio

---
+++ static/script/desplazamiento.ts
@@ -0,0 +1,18 @@
+//@ts-nocheck
+const links = document.getElementsByClassName("desplazamiento");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
